6ad25ade89e3d042f5591c0b8ac7361d50932153
Diogo-Felipe <diogo.felipe.praxent@gopangea.com>
2024-10-29 16:12:35 -0300

    objects

---
+++ 5.objetos.js
@@ -0,0 +1,13 @@
+const pessoa = {
+  nome: "Diogo Felipe",
+  idade: 27,
+  ehMaior: true,
+  profissao: "Programador",
+};
+
+console.log(pessoa.nome);
+console.log(pessoa.idade);
+
+const atributoParaAcessar = "profissao";
+console.log(pessoa[atributoParaAcessar]);
+

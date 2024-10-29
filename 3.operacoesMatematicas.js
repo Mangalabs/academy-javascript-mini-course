10d0f9b3a8c51ca41ae947e76b1bd8780be378fc
Diogo-Felipe <diogo.felipe.praxent@gopangea.com>
2024-10-29 16:12:21 -0300

    math operations

---
+++ 3.operacoesMatematicas.js
@@ -0,0 +1,20 @@
+const a = 2;
+const b = 3;
+
+const adicao = a + b;
+console.log("adicao", adicao);
+
+const subtracao = a - b;
+console.log("subtracao", subtracao);
+
+const multiplicacao = a * b;
+console.log("multiplicacao", multiplicacao);
+
+const divisao = a / b;
+console.log("divisao", divisao);
+
+const resto = a % b;
+console.log("resto", resto);
+
+const expoente = a ** b;
+console.log("expoente", expoente);

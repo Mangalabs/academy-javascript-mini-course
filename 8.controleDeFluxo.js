b3141056a5c1b7c7c581a512d3d718ab959155e9
Diogo-Felipe <diogo.felipe.praxent@gopangea.com>
2024-10-29 16:12:56 -0300

    ifs

---
+++ 8.controleDeFluxo.js
@@ -0,0 +1,15 @@
+const idade = 7;
+
+if (idade < 18) {
+  console.log("Usuario menor de idade");
+} else {
+  console.log("Usuario maior de idade");
+}
+
+if (idade < 12) {
+  console.log("Criança");
+} else if (idade < 18) {
+  console.log("Adolescente");
+} else {
+  console.log("Adulto");
+}

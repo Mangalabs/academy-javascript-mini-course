29b1b10f817f71b5666d4ed7d33152ebfeb655e1
Diogo-Felipe <diogo.felipe.praxent@gopangea.com>
2024-10-29 16:12:15 -0300

    data types

---
+++ 2.tiposDeDados.js
@@ -0,0 +1,13 @@
+let idade = 12; //number
+let texto = "Esse é um texto"; //string
+let estaChovendo = false; //boolean
+let naoSeiOqueColocarAqui = undefined; //undefined
+let vencedorDaCorrida = null; //null
+let numeroMuitoGrande = 123123123123123123123123123n; //bigInt
+
+console.log("idade", typeof idade);
+console.log("texto", typeof texto);
+console.log("estaChovendo", typeof estaChovendo);
+console.log("naoSeiOqueColocarAqui", typeof naoSeiOqueColocarAqui);
+console.log("vencedorDaCorrida", typeof vencedorDaCorrida);
+console.log("numeroMuitoGrande", typeof numeroMuitoGrande);
